e529f513d81405ab21a122fdc2e10818c3aebcd5
asabeh11 <asabeh59@gmail.com>
2021-02-24 17:25:20 -0500

    changed speed

--- shea_main.go
+++ shea_main.go
@@ -57,16 +57,16 @@ func robotRunLoop(lightSensor *aio.GroveLightSensorDriver, soundSensor *aio.Grov
 			gpg.SetMotorDps(g.MOTOR_RIGHT, 0)
 			gpg.SetMotorDps(g.MOTOR_LEFT, 0)
 		} else if !lightFound && !calibrated {
-			gpg.SetMotorDps(g.MOTOR_RIGHT, 30)
+			gpg.SetMotorDps(g.MOTOR_RIGHT, 40)
 		} else if lightFound {
-			gpg.SetMotorDps(g.MOTOR_RIGHT, 30)
-			gpg.SetMotorDps(g.MOTOR_LEFT, 30)
+			gpg.SetMotorDps(g.MOTOR_RIGHT, 40)
+			gpg.SetMotorDps(g.MOTOR_LEFT, 40)
 		}
 		if val > encode_vals[1]+1275 {
 			rotation = true
 		}
 		if rotation && !lightFound {
-			gpg.SetMotorDps(g.MOTOR_RIGHT, -30)
+			gpg.SetMotorDps(g.MOTOR_RIGHT, -40)
 			if val <= m[maxNumber(m)] {
 				lightFound = true
 				calibrated = true
